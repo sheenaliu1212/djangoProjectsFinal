994ada4788ed86eadf910a501e463ed60755e99c
liuhsin <s111113209@stu.ntue.edu.tw>
2025-05-12 23:41:56 +0800

    test

--- gamaeshop/gamaeshop/urls.py
+++ gamaeshop/gamaeshop/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import company.views as coViews
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('company/index/', coViews.index, name='index'),
 ]
